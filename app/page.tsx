9b11fe323f99cef75a438016f34dc68629b91cea
surajarya130 <surajarya130@gmail.com>
2025-01-03 23:25:03 +0530

    WIP Coding challenge

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 
 export default function Home() {
+  function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14,
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch (ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+  console.log(PI, "srj ");
+
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
