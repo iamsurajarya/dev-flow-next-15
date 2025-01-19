2d5525b27194817c1b9252d42300fd67f954362b
surajarya130 <surajarya130@gmail.com>
2025-01-19 22:30:59 +0530

    WIP- Fixing Streak

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,21 +23,6 @@ export const metadata: Metadata = {
   },
 };
 
-export const metadata2: Metadata = {
-  title: "Dev Flow",
-  description: "Another way to stack-over-flow",
-  icons: {
-    icon: "/images/site-logo.svg",
-  },
-};
-export const metadata3: Metadata = {
-  title: "Dev Flow",
-  description: "Another way to stack-over-flow",
-  icons: {
-    icon: "/images/site-logo.svg",
-  },
-};
-
 export default function RootLayout({
   children,
 }: Readonly<{
