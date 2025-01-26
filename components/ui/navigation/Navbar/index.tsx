f24f319df0a601bb2f8ea61a4abea08c58aee3c2
surajarya130 <surajarya130@gmail.com>
2025-01-26 23:35:15 +0530

    WIP Navbar

--- components/ui/navigation/Navbar/index.tsx
+++ components/ui/navigation/Navbar/index.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full p-6 dark:shadow-none sm:px-12 shadow-light-300 gap-5">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/images/site-logo.svg" width={23} height={23} alt="dev-flow" />
+        <p>
+          Dev <span>Flow</span>
+        </p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Navbar;
